b698e970308c94040200e077cfb18b1afd3ee5b9
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-03 23:48:30 +0000

    define attributes for clients models

--- api/models/clients.js
+++ api/models/clients.js
@@ -1,7 +1,108 @@
 const Client = async (sequelize, data_type)=>{
   await sequelize.define('clients', {
-    // Model attributes are defined here
-    firstName: {
+    // Model attributes
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+    first_name: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    middle_name: {
+      type: data_type.STRING,
+    },
+    last_name: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    date_of_birth: {
+      type: data_type.DATE,
+      allowNull: false
+    },
+    salary_deposit_to_mobile_money: {
+      type: data_type.BOOLEAN,
+      allowNull: false
+    },
+    monthly_salary: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    home_address: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    employment_contract: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    employment_letter: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    government_issued_id: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    id_card_type: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    gurantor_fullname: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    gurantor_contact_number: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    gurantor_home_address: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    gurantor_relation_to_creditor: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    mou_from_gurantor: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    gurantor_government_issued_id: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    hr_fullname: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    hr_contact_number: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    employer_name: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    employer_address: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    loan_limit: {
+      type: data_type.STRING,
+      allowNull: false
+    },
+    validation_process_complete: {
+      type: data_type.BOOLEAN,
+      allowNull: false
+    },
+    pass_validation: {
+      type: data_type.BOOLEAN,
+      allowNull: false
+    },
+    terms_and_condition_accepted: {
       type: data_type.STRING,
       allowNull: false
     },
@@ -9,8 +110,6 @@ const Client = async (sequelize, data_type)=>{
       type: data_type.STRING
       // allowNull defaults to true
     }
-  }, { 
-    // Other model options go here 
   });
 
 }
