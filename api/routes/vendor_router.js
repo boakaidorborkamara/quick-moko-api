06f80f7f609afd25b54478cd43036e7922eaad8b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-06 01:38:14 +0000

    Add more endpoints to vendor router file

--- api/routes/vendor_router.js
+++ api/routes/vendor_router.js
@@ -14,5 +14,17 @@ router.post('/api/v1/vendors', vendorController.vendor_create);
 router.get('/api/v1/vendors', vendorController.vendor_list);
 
 
+//Handle display of details for specific vendor on GET
+router.get('/api/v1/vendors/:id', vendorController.vendor_details);
+
+
+// Handle edit of specific vendor details on PUT 
+router.put('/api/v1/vendors/:id', vendorController.vendor_edit);
+
+
+//Handle delete of specific vendor on DELETE
+router.delete('/api/v1/vendors/:id', vendorController.vendor_delete);
+
+
 
 module.exports = router;
