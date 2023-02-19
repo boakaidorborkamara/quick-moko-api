bc89ca2bfd666da91071094e7f93985e9c3226ff
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-19 23:15:58 +0000

    Organize message object in sendSmsNotificationsfunction

--- api/helper/sendSMS.js
+++ api/helper/sendSMS.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 
 const sendSmsNotifications = (receipt_number, message)=>{
 
-    console.log("working");
-
+    // generate message id 
     let message_id = 0;
 
+    // configure message 
     let new_message = {
         senderName: "Sogital Lab",
         messages: [
@@ -18,28 +18,23 @@ const sendSmsNotifications = (receipt_number, message)=>{
         ]
     }
 
-    axios.post('https://httpbin.org/post', {
+    // convert new message object to json 
+    let converted_new_message = JSON.stringify(new_message);
 
-        "senderName": "Kwagei",
-        "messages": [
-          {
-            "to": "0776001001",
-            "body": "Hello world!",
-            "extMessageId": "<extMessageId>"
-          }
-        ]
-      }, {
+
+    //send new sms
+    axios.post('https://developer.lonotalk.com/api/v1/sms', converted_new_message, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'apiKey': 'tCvqgcZp0faUyBWRzbCln7O6H6SoeSnhT5A6L57SeZRnaUe2Dkce6yZyYBo5xXXU'
         }
     })
-      .then(function (response) {
+    .then(function (response) {
         console.log(response);
-      })
-      .catch(function (error) {
+    })
+    .catch(function (error) {
         console.log(error);
-      });
+    });
 }
 
 
