6f7525c9c68f9795dded007e140f9c1c3fa5903b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-22 04:20:24 +0000

    Construct a function that creates api key for api user on sandbox

--- api/helper/createAPiUsers.js
+++ api/helper/createAPiUsers.js
@@ -67,4 +67,39 @@ let validateAPIuser = async ()=>{
 }
 
 
-module.exports = {createAPIuser};
+// Implement creation of api key for api user in the sandbox 
+let createAPIkey = async ()=>{
+
+    // make a post request to sandbox while providing all details needed for a sucessful request 
+    axios.post(`${baseUrl}/v1_0/apiuser/${x_refrence_id}/apikey`, {
+        
+      },{
+        headers:{
+
+            "Ocp-Apim-Subscription-Key": process.env.DISBURSEMENT_PRIMARY_KEY,
+            
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+
+        // return 0 if reques was sucessful 
+        if(response.data){
+            return 0
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+        // return 1 if request wasn't sucessful 
+        return 1
+    });
+
+
+}
+
+
+module.exports = {
+    createAPIuser,
+    validateAPIuser,
+    createAPIkey
+};
