d1f963f51055583293fe535572e49346b1dde98a
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-02-22 03:55:12 +0000

    Construct a function taht validate api user

--- api/helper/createAPiUsers.js
+++ api/helper/createAPiUsers.js
@@ -3,9 +3,10 @@ const { v4: uuidv4 } = require('uuid');
 
 
 let baseUrl = "https://sandbox.momodeveloper.mtn.com";
+let x_refrence_id  = uuidv4();
 
 
-
+// Implement creation of new api user 
 let createAPIuser = async ()=>{
 
     // make a post request to sandbox while providing all details needed for a sucessful request 
@@ -13,7 +14,7 @@ let createAPIuser = async ()=>{
         "providerCallbackHost": "quickmoko.com"
       },{
         headers:{
-            "X-Reference-Id": uuidv4(),
+            "X-Reference-Id": x_refrence_id,
             "Ocp-Apim-Subscription-Key": "9078543234db4189ab147b6cb62e3199",
             
         }
@@ -33,6 +34,35 @@ let createAPIuser = async ()=>{
     });
 
 
+}
+
+
+// Validates if API user was actually created 
+let validateAPIuser = async ()=>{
+
+    // make a post request to sandbox while providing all details needed for a sucessful request 
+    axios.get(`${baseUrl}/v1_0/apiuser/${x_refrence_id}`, {
+        "providerCallbackHost": "quickmoko.com"
+      },{
+        headers:{
+
+            "Ocp-Apim-Subscription-Key": "9078543234db4189ab147b6cb62e3199",
+            
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+
+        // return 0 if reques was sucessful 
+        if(response.data){
+            return 0
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+        // return 1 if request wasn't sucessful 
+        return 1
+    });
 }
 
 
